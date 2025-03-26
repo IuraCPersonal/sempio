390271c92914cb981a5a4e384f6eaab055f2390b
IuraCPersonal <iurie.cius.personal@gmail.com>
2025-03-26 22:03:57 +0200

    chore: serve minimal hono server

--- packages/sempio/src/cli.ts
+++ packages/sempio/src/cli.ts
@@ -1,1 +1,10 @@
-console.log("Hello, from sempio 🌿")
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+serve(app);
